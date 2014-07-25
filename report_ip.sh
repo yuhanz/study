38439b7f7863e286eb53d2d1e3bb38b6287a0f49
yuhan <yuhan@acer.(none)>
2014-07-25 01:00:18 -0700

    report ip

--- report_ip.sh
+++ report_ip.sh
@@ -1,3 +1,3 @@
-wget 'http://www.whatsmyip.org/' -o ip.html
+wget ipchicken.com -O ip.html --user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36"
 git add . && git commit -a -m "report ip"
 git push
