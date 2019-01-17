7510114024bd15377c3012cf802f26c490440ced
Zhang Yuhan <yuhanz@gmail.com>
2019-01-17 00:56:12 -0800

    lunar landing: punish large negative result; punish long running of negative result

--- train.py
+++ train.py
@@ -28,13 +28,29 @@ for j in  range(1,NUM_EPISODES):
     NUM_RECORDS = 300
 
     positive_actual_reward = 0
+    negatives_actual_reward = 0
+    reward_streak = 0    # how many negative reward appeared in a row? positive value if positive rewards in a row; negative value if negative rewards in a row
+
+    wheel_touchdown_reward = 0
 
     for i in range(1,NUM_RECORDS):
       observation = observation_next
-      target_rewards, observation_next, done, actual_reward = gym_app.step_and_collect_data(env, observation, sess, input, output, lambda max_future_reward, reward, env: reward + positive_actual_reward + (20 if observation[6] and observation[7] else -15 if observation[6] or observation[7] else -10))
+
+      num_wheels_touchdown = observation[6] + observation[7];
+      wheel_touchdown_reward = 50 if num_wheels_touchdown >= 2 else (num_wheels_touchdown-2) * 50
+
+      target_rewards, observation_next, done, actual_reward = gym_app.step_and_collect_data(env, observation, sess, input, output, lambda max_future_reward, reward, env: reward + positive_actual_reward + negatives_actual_reward + wheel_touchdown_reward)
       print "target_rewards:", target_rewards
 
+      if actual_reward > 0:
+        reward_streak = 1 if reward_streak < 0 else reward_streak + 1
+      if actual_reward < 0:
+        reward_streak = -1 if reward_streak > 0 else reward_streak - 1
+
       positive_actual_reward += actual_reward if actual_reward > 0 else 0
+      negatives_actual_reward += actual_reward if actual_reward < -4 else 0    # punish large negative values
+      negatives_actual_reward += actual_reward if reward_streak < -3 else 0     # punish long period of negative values
+
       records.append([observation, target_rewards, actual_reward])
       if(done):
         print "=== done"
