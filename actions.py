e37282d47a891a3d762f6acc8edae47ddb68b01a
Yuhan Zhang <yuhanz@gmail.com>
2019-02-01 00:59:53 -0800

    initial setup of rasa chatbot

---
+++ actions.py
@@ -0,0 +1,19 @@
+from rasa_core_sdk import Action
+from rasa_core_sdk.events import SlotSet
+
+import requests
+
+class ActionGetGreatPrice(Action):
+   def name(self):
+      # type: () -> Text
+      return "action_get_great_price"
+
+   def run(self, dispatcher, tracker, domain):
+      # type: (CollectingDispatcher, Tracker, Dict[Text, Any]) -> List[Dict[Text, Any]]
+
+      model = tracker.get_slot('model')
+      url = 'https://qa-11-www.edmunds.com/api/inventory/v5/find/?model={}&fields=prices,thirdPartyInfo'.format(model)
+      resp = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'})
+      result = resp.json()
+      greatPrice = result.get('results')[0].get('thirdPartyInfo').get('priceValidation').get('maxGreatPrice')
+      return [SlotSet("greatPrice", '{}'.format(greatPrice))]
---
+++ stories.md
@@ -0,0 +1,29 @@
+## happy path
+* greet
+  - utter_greet
+  - slot{"model": "Accord"}
+* mood_great
+  - utter_happy
+  - action_get_great_price
+
+## sad path 1
+* greet
+  - utter_greet
+* mood_unhappy
+  - utter_cheer_up
+  - utter_did_that_help
+* mood_affirm
+  - utter_happy
+
+## sad path 2
+* greet
+  - utter_greet
+* mood_unhappy
+  - utter_cheer_up
+  - utter_did_that_help
+* mood_deny
+  - utter_goodbye
+
+## say goodbye
+* goodbye
+  - utter_goodbye
