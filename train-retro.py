e61eabd6c4924a7ea0cbbaec96fa01d3faf28e0f
Zhang Yuhan <yuhanz@gmail.com>
2019-03-29 22:18:52 -0700

    abstract megaman move control into (brake, steer, press_gas)

--- train-retro.py
+++ train-retro.py
@@ -8,16 +8,35 @@ env = retro.make(game='MegaMan2-Nes', record='.')
 
 previous_screen = env.reset()
 
+# either left or right
+actionRight = np.array([0, 0, 0, 0, 0, 0, 0, 1, 0])  # forward
+actionLeft = np.array([0, 0, 0, 0, 0, 0, 1, 0, 0])  # backward
+
+# either jump or release
+actionJump = np.array([0, 0, 0, 0, 0, 0, 0, 0, 1]) # jump
+
+# either fire or release
 actionFire = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0])  # fire
-actionForward = np.array([0, 0, 0, 0, 0, 0, 0, 1, 0])  # forward
-#action1 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 1]) # jump
+
 actionRelease = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0]) # release
 #action = env.action_space.sample()
 
-frames = 200
+frames = 30
+
+# basic inputs
+# [brake, steer, press_fuel]
+# brake: if brake > 0: stop moving left or right
+#       if brake <= 0: let move decide direction
+# steer : if steer > 0: move left
+#        if steer <= 0: move right
+# press_fuel: adding this amount to jump_fuel. This amount can be negative
+
+jump_fuel = 0   # when jump fuel is none zero. keep holding the jump
+
+action = actionRelease
 
 for i in range(1,frames):
-    action = np.logical_or(actionForward, [actionFire, actionRelease][i%2])
+
     # print(action)
     _obs, _rew, done, _info = env.step(action)
 
@@ -26,6 +45,19 @@ for i in range(1,frames):
     if i % 4 == 0:
         shiftValue = detection.detect_horizontal_shift_all_rows(previous_screen, _obs)
         print('shift: ', shiftValue)
+
+        # TODO: decide [brake, steer, press_fuel]
+        [brake, steer, press_fuel] = [0, -1, 0]
+        jump_fuel += press_fuel
+
+        jump = actionJump * [1 if jump_fuel > 0 else 0]
+        fire = actionFire * (i%2)
+        left = actionLeft * (steer > 0)
+        right = actionRight * (steer <= 0)
+        action = fire + left + right + jump
+
+        jump_fuel = 0 if jump_fuel <= 0 else jump_fuel - 1
+
         previous_screen = _obs
-        # img = Image.fromarray(_obs, 'RGB')
-        # img.show()
+        img = Image.fromarray(_obs, 'RGB')
+        img.show()
